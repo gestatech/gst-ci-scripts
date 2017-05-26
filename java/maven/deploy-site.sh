e3bc0f362c6d69c77613e470d31f29be97d57214
Gestatech <fariala.amuri@gestatech.be>
2017-05-26 23:32:00 +0200

    [GST-CI-S] Update deploy-site.sh

--- java/maven/deploy-site.sh
+++ java/maven/deploy-site.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+#
+# Function for deploying the project Maven site. A flow flag is received, indicating
+# if the deployment should be done or not.
+#
+# Deployment should be done thoughtfully. Make sure everything is ready before
+# running this script.
+#
+# If everything is correct, the deployment will only occur with release or development
+# versions. Any pull request, in case the code comes from SCM, will be ignored.
+#
+# Also a settings file will be read from the ~/settings.xml path.
+#
+# -- PARAMETERS --
+#
+# The function expects the following parameters:
+# $1: A flow control boolean flag, indicating if the script should be run or not.
+# $2: A comma separated list of profiles, or an empty string for no profile.
+#
+
 # Fails if any used variable is not set
 set -o nounset
 # Fails if any commands returns a non-zero value
